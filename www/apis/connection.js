3e96a94eff3166c812d1084a1c723e58dad8fdcb
specindia2013 <specindia2013@gmail.com>
2013-04-19 14:39:57 +0530

    Create connection.js

---
+++ www/apis/connection.js
@@ -0,0 +1,16 @@
+
+// api-connection
+function check_network() {
+    var networkState = navigator.network.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.NONE]     = 'No network connection';
+
+    $('#connection').html(states[networkState]);
+}
